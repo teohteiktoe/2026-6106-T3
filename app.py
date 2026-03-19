c68c06af62fb84c89ac354278aea1d805fa5d986
Teoh TeikToe <teohteiktoe@gmail.com>
2026-03-19 11:25:12 +0000

    1

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route("/",methods=["GET","POST"])
 def index():
-    return("<h2>Hi</h2>")
+    return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    return(render_template("main.html"))
+
+@app.route("/transferMoney",methods=["GET","POST"])
+def transferMoney():
+    return(render_template("transferMoney.html"))
 
 if __name__ == "__main__":
         app.run()
